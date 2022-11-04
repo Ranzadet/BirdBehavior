7f373cbd9996fcf1d674068d941e2b389c50af2d
Benen Sullivan <benensullivan8@gmail.com>
2022-11-03 23:45:14 -0400

    Finished HOG implementation
    features is an array of histograms which can be fed into an SVM if supervised, otherwise support vector clustering model?

--- BirdModel.py
+++ BirdModel.py
@@ -90,16 +90,32 @@ while cap.isOpened():
     #At this point, histograms should contain every histogram for each cell
     #Next, we need to normalize each block to account for lighting variance
     blockSize = (16, 16)
+    blocks = []
 
-    #combine each 9x1 histogram vector in the block into a cell*9 X 1 matrix
+    #combine each 9x1 histogram vector in the block into a cells*9 X 1 matrix
     #then, normalize the matrix by dividng each value by the vector's L2 norm (root of sum of squares)
+    for i in range((len(nframe)//cellWidth)-1):
+        for j in range((len(nframe[0]) // cellHeight)-1):
+            block = np.array(histograms[i] + histograms[i+1] + histograms[2] + histograms[j+1])
+            L2 = np.sqrt(sum(a**2 for a in block))
+            for b in range(len(block)):
+                block[b] = block[b] / L2
+            blocks.append(block)
 
     #k = np.sqrt(sum(a**2 for a in vector))
     #for a in range(len(vector)):
     #   vector[a] = vector[a] / k
 
     #Finally,flatten into a 1-dimensional feature matrix (which is usually fed into a SVM)
-    #features = np.reshape(blockVectors, len(blockVectors) * len(blockVectors[0]))
+    features = np.reshape(blocks, len(blocks) * len(blocks[0]))
+    #features.shape = 9 * cellsPerBlock * blocksPerImage
+    #print(features.shape)
+######################################################################################
+    #TODO: IMPLEMENT Neural Network - SUPPORT VECTOR MACHINE (support vector clustering?)
+    #Run entire video and write feature vector of each frame directly into a text file / spreadsheet?
+
+
+
 ######################################################################################
 
 
